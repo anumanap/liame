20e0c80a1cfc20260a4da15c50eb5f933d57e92d
anumanap <156611434+anumanap@users.noreply.github.com>
2024-04-16 16:25:16 +0800

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for Redmi Redmi 9 (lancelot)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lancelot)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
